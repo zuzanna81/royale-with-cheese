301bdb6225fef72c486e0c3fef32248bf2214374
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-12-06 15:58:51 +0100

    Clean up user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,3 @@
 class UsersController < ApplicationController
-  def show
-
-  end
-
-  def create
   
-  end
-
-  def new
-
-  end
-
-  private
-
-  def user_params
-    params[:user].permit(:email, :password, :password_confirmation)
-  end
 end
