468312544c7ae739f9ab63836195597fc0d92d38
Francesco <francesco.falchi@gmail.com>
2017-12-06 14:50:31 +0100

    Define search method and results in index

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,13 @@
 class LandingController < ApplicationController
   def index
+    # @movies = Tmdb { results:[] }
+  end
+
+  def search
+    genre = params[:genre].capitalize
+    movie_genre = Tmdb::Genre.movie_list.select{ |m| m.name == genre}.first
+    @movies = Tmdb::Discover.movie(sort_by: 'popularity.desc', with_genres: "#{movie_genre.id}")
+
+    redirect_to root_path
   end
 end
