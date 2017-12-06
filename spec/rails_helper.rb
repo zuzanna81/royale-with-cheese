ede0024fa7de268c3e606969c87afbc255c2d869
ScottB <17sb@cis.dk>
2017-12-06 21:34:56 +0100

    Modify rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,7 +13,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
 end
 
-Shoulda::Matchers.configure do |config|
+Shoulda::Matchers.configure do |config| 
   config.integrate do |with|
     with.test_framework :rspec
     with.library :rails
