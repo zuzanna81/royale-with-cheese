32df542d340d1423945047a3bd3ece65ad9219db
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-12-07 20:17:05 +0100

    Implement if statement in controller, all specs pass

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,10 +3,15 @@ class LandingController < ApplicationController
   end
 
   def search
+
     genre = params[:genre].capitalize
     movie_genre = Tmdb::Genre.movie_list.select{ |m| m.name == genre }.first
-    @movies = Tmdb::Discover.movie(sort_by: 'popularity.desc', with_genres: "#{movie_genre.id}")
+    @movies = Tmdb::Discover.movie(sort_by: 'popularity.desc', with_genres: "#{movie_genre.id}") if movie_genre
 
+    if @movies.nil?
+      flash[:notice] = 'wrong entry'
+
+    end
     render :index
   end
 end
