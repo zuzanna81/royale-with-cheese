9e922057c0faa5ccd9127efea8d82e54cd2b36a1
Raoul DIFFOUO <diraulo@gmail.com>
2017-12-07 14:45:53 +0200

    Move @tmdb hook to hooks file

--- features/support/env.rb
+++ features/support/env.rb
@@ -15,17 +15,3 @@ end
 Cucumber::Rails::Database.javascript_strategy = :truncation
 
 World(FactoryBot::Syntax::Methods)
-
-Before("@tmdb") do
-  genres_path = "#{::Rails.root}/spec/fixtures/genres_response_body.txt"
-  file = File.open(genres_path)
-  stub_request(:get, "http://api.themoviedb.org/3/genre/movie/list?api_key=#{ENV['TMDB_API_KEY']}").
-    with(headers: {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip, deflate', 'Content-Type'=>'application/json; charset=utf-8', 'Host'=>'api.themoviedb.org', 'User-Agent'=>'rest-client/2.0.2 (darwin15.5.0 x86_64) ruby/2.4.1p111'}).
-    to_return(status: 200, body: file.read, headers: {})
-
-  movies_path = "#{::Rails.root}/spec/fixtures/movies_response_body.txt"
-  file = File.open(movies_path)
-  stub_request(:get, "http://api.themoviedb.org/3/discover/movie?api_key=#{ENV['TMDB_API_KEY']}&sort_by=popularity.desc&with_genres=28").
-    with(headers: {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip, deflate', 'Content-Type'=>'application/json; charset=utf-8', 'Host'=>'api.themoviedb.org', 'User-Agent'=>'rest-client/2.0.2 (darwin15.5.0 x86_64) ruby/2.4.1p111'}).
-    to_return(status: 200, body: file.read, headers: {})
-end
