8f843a384a77d0bf98368c6e069439cafecbb1fc
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-12-07 09:19:01 +0100

    Remove binding pry

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -5,7 +5,6 @@ class LandingController < ApplicationController
 
   def search
     genre = params[:genre].capitalize
-    binding.pry
     movie_genre = Tmdb::Genre.movie_list.select{ |m| m.name == genre }.first
     @movies = Tmdb::Discover.movie(sort_by: 'popularity.desc', with_genres: "#{movie_genre.id}")
 
